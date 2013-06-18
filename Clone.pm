3c9aba54248814344cc54b650daf0a7fb42baf7c
Mark Fowler <mark@twoshortplanks.com>
2013-06-18 12:45:02 -0400

    change module name, description

--- Clone.pm
+++ Clone.pm
@@ -1,4 +1,4 @@
-package Clone;
+package Clone::AsUTF8Bytes;
 
 use strict;
 use Carp;
@@ -13,11 +13,11 @@ require AutoLoader;
 # names by default without a very good reason. Use EXPORT_OK instead.
 # Do not simply export all your public functions/methods/constants.
 @EXPORT = qw();
-@EXPORT_OK = qw( clone );
+@EXPORT_OK = qw( clone_as_utf8_bytes );
 
 $VERSION = '0.34';
 
-bootstrap Clone $VERSION;
+__PACKAGE__->bootstrap($VERSION);
 
 # Preloaded methods go here.
 
@@ -28,38 +28,39 @@ __END__
 
 =head1 NAME
 
-Clone - recursively copy Perl datatypes
+Clone::AsUTF8Bytes - recursively copy Perl data converting to UTF-8 bytes
 
 =head1 SYNOPSIS
 
   package Foo;
-  use parent 'Clone';
+  use parent 'Clone::AsUTF8Bytes';
 
   package main;
   my $original = Foo->new;
-  $copy = $original->clone;
+  $copy = $original->clone_as_utf8_bytes;
   
   # or
 
-  use Clone qw(clone);
+  use Clone::AsUTF8Bytes qw(clone_as_utf8_bytes);
   
-  $a = { 'foo' => 'bar', 'move' => 'zig' };
-  $b = [ 'alpha', 'beta', 'gamma', 'vlissides' ];
+  $a = { 'M\x{f8}\x{f8}se' => "\x{1F44D}" };
+  $b = [ "L\x{e9}on', "\N{SNOWMAN}" ];
   $c = Foo->new;
 
-  $d = clone($a);
-  $e = clone($b);
-  $f = clone($c);
+  $d = clone_as_utf8_bytes($a);
+  $e = clone_as_utf8_bytes($b);
+  $f = clone_as_utf8_bytes($c);
 
 =head1 DESCRIPTION
 
-This module provides a clone() method which makes recursive
-copies of nested hash, array, scalar and reference types, 
-including tied variables and objects.
+This module provides a clone_as_utf8_bytes() method which makes recursive copies
+of nested hash, array, scalar and reference types, including tied variables and
+objects, modifying the characters in the strings as it does so into UTF-8 bytes.
+For example the Perl string C<L\x{e9}on> in the data strucutre will be converted
+to the string C<L\x{c3}\x{a9}on>.
 
-
-clone() takes a scalar argument and duplicates it. To duplicate lists,
-arrays or hashes, pass them in by reference. e.g.
+clone_as_ut8_bytes() takes a scalar argument and duplicates and modifies it. To
+duplicate lists, arrays or hashes, pass them in by reference. e.g.
     
     my $copy = clone (\@array);
 
@@ -69,25 +70,27 @@ arrays or hashes, pass them in by reference. e.g.
 
 =head1 SEE ALSO
 
-L<Storable>'s dclone() is a flexible solution for cloning variables,
-albeit slower for average-sized data structures. Simple
-and naive benchmarks show that Clone is faster for data structures
-with 3 or less levels, while dclone() can be faster for structures
-4 or more levels deep.
+This module is essentially the L<Clone> module slightly altered to do the
+utf-8 byte conversion.
 
 =head1 COPYRIGHT
 
-Copyright 2001-2012 Ray Finch. All Rights Reserved.
+Copyright 2013 OmniTI.  All Rights Reserved.
+
+Majority of code in this module copyright 2001-2012 Ray Finch. All Rights
+Reserved.
 
 This module is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
 
 =head1 AUTHOR
 
-Ray Finch C<< <rdf@cpan.org> >>
+Coversion from Clone to Clone::AsUTF8Bytes was performed by Mark Fowler.
+C<< <mark@twoshortplanks.com> >>.
 
-Breno G. de Oliveira C<< <garu@cpan.org> >> and
-Florian Ragwitz C<< <rafl@debian.org> >> perform routine maintenance
-releases since 2012.
+Original Clone module - which makes up the overall majority of the module - was
+written by Ray Finch C<< <rdf@cpan.org> >>.  Breno G. de Oliveira C<<
+<garu@cpan.org> >> and Florian Ragwitz C<< <rafl@debian.org> >> performed
+routine maintenance releases on the Clone module since 2012.
 
 =cut
